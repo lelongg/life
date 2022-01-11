5913216b7c66e8454a6326eef8831389cb2d13a1
Gérald Lelong <gerald.lelong@easymov.fr>
2022-01-11 19:56:11 +0100

    working

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
-  description = "A very basic flake";
-
   outputs = { self, nixpkgs }:
-    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    with import nixpkgs { system = "x86_64-linux"; };
+    let
+      build = writeShellScriptBin "build.sh" ''
+        cmake -B build -DCMAKE_INSTALL_PREFIX="~/.local"
+        make -C build
+      '';
+      install = writeShellScriptBin "install.sh" ''
+        make -C build install
+      '';
+
     in with pkgs; {
       devShell.x86_64-linux = mkShell {
-        buildInputs = [
-          cmake
-          zsh
-          extra-cmake-modules
-          libsForQt5.plasma-sdk
-          libsForQt5.ki18n
-        ];
+        buildInputs = [ cmake zsh extra-cmake-modules appstream build install ]
+          ++ (with libsForQt5; [ plasma-framework ki18n ]);
       };
     };
 }
